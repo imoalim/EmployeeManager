1a6bef4d867e50927982492c152834ba40bd738c
imoalim <113512259+imoalim@users.noreply.github.com>
2024-02-20 14:19:08 +0100

    fixed naming

--- src/main/java/com/learn/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/com/learn/employeemanager/repo/EmployeeRepo.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 
-    void deleteEmployeeByID(Long id);
+    void deleteEmployeeByid(Long id);
 
-    Optional<Employee> findEmployeeById(Long id);
+    Optional<Employee> findEmployeeByid(Long id);
 }
--- src/main/java/com/learn/employeemanager/service/EmployeeService.java
+++ src/main/java/com/learn/employeemanager/service/EmployeeService.java
@@ -28,10 +28,10 @@ public class EmployeeService {
         return employeeRepo.save(employee);
     }
     public Employee findEmployeeById(Long id){
-        return employeeRepo.findEmployeeById(id).orElseThrow(()
+        return employeeRepo.findEmployeeByid(id).orElseThrow(()
                 -> new UserNotFoundException("User by id " + id + " was not found"));
     }
     public void deleteEmployee(Long id){
-        employeeRepo.deleteEmployeeByID(id);
+        employeeRepo.deleteEmployeeByid(id);
     }
 }
