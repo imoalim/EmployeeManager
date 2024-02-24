b49b2821e13f66e256fce89c0820d01418e4ea4d
imoalim <113512259+imoalim@users.noreply.github.com>
2024-02-24 11:03:07 +0100

    backend speaks with the frontend

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Employee} from "./employee";
+import {EmployeeService} from "./employee.service";
+import {HttpErrorResponse} from "@angular/common/http";
+import {Observable, Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeeManagerApp';
+export class AppComponent implements OnInit{
+  //title = 'employeeManagerApp';
+  employees$!: Observable<Employee[]>;
+
+
+  //access to the service because we have injected it to this AppComponent
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(): void {
+    this.employees$ = this.employeeService.getEmployee();
+  }
+  private destroy$ = new Subject<void>();
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
 }
